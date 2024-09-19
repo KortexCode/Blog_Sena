e0f63922d461f7a93ee4021b2de50295e4da3cbf
KortexDev <cacortesl01@gmail.com>
2024-09-19 14:49:11 -0500

    Se corrigen ruta del index.jsx en el index.html

--- src/routes/ExposureBlog.jsx
+++ src/routes/ExposureBlog.jsx
@@ -6,7 +6,7 @@ function ExposureBlog() {
         <section className='exposureblog'>
             <h2>Product evidence: GA2-240202501-AA2-EV02: exposure.</h2>
             <p>In accordance with the topic studied in the third level, make a presentation in English in video format about a tourist spot in your region, through a short video using your webcam, in order to orally identify the description and the reason for the popularity of the town or city.</p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/o1i3cxkxr8I?si=GARhGZh_w5MJAPAT" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/o1i3cxkxr8I?si=GARhGZh_w5MJAPAT" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             
         </section>
     )
--- src/routes/VideoBlog.jsx
+++ src/routes/VideoBlog.jsx
@@ -6,7 +6,7 @@ function VideoBlog() {
         <section className='videoblog'>
             <h2>Product evidence: GA1-240202501-AA1-EV02 Video</h2>
             <p>In accordance with the topic studied in the first level, choose a family member or a friend to introduce them through a short video using your webcam in English, in order to orally identify personal characteristics such as age, nationality, place of residence, hobbies, and some relevant information about their daily activities.</p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/2ctVh_BtUW8?si=GQJt0Tj7J5O4yI77" title="Presentation" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2ctVh_BtUW8?si=GQJt0Tj7J5O4yI77" title="Presentation" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
     )
 }
