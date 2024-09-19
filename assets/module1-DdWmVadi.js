6319ee402e68435755e00f375b4ba6db06b33be7
KortexDev <cacortesl01@gmail.com>
2024-09-19 14:50:22 -0500

    Updates

---
+++ assets/module1-DdWmVadi.js
@@ -0,0 +1,1 @@
+function o(){console.log("cargando módulo 1")}const l={name:"Aquí está el módulo 1"};export{o as load,l as module1};
---
+++ assets/module2-GxO2LClB.js
@@ -0,0 +1,1 @@
+function o(){console.log("cargando módulo 2")}const l={name:"Aquí está el módulo 2"};export{o as load,l as module2};
