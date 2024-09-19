496a1b445249b29c72ad24765144ffdd6785fee1
KortexDev <cacortesl01@gmail.com>
2024-09-19 15:27:53 -0500

    cambios en el header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header(){
     return(
         <header>
             <h1>Welcome to my blog</h1>
-            <p>Activities about the english componet</p>
+            <p>Activities about the english component</p>
         </header>
     )
 }
