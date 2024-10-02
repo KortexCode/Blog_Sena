b5cede7cdaf248600b9bf660c4426ff6dccf7daa
KortexDev <cacortesl01@gmail.com>
2024-10-02 18:45:21 -0500

    Se cambia el título del header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@
 function Header(){
     return(
         <header>
-            <h1>Welcome to my blog</h1>
+            <h1>Welcome</h1>
             <p>Activities about the english component</p>
         </header>
     )
